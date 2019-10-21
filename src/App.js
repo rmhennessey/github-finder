5ae745f693197538c698eca942935116ef4b27af
RMH <rory.hennessey@gmail.com>
2019-10-21 17:37:40 -0400

    navbar component created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>Welcome to the Jungle BOIII</h1>
+        <Navbar />
       </div>
     );
 
